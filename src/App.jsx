3efc7ab1d61c80c14758347f2ae9c6dd3a5333c9
shandiru <msddiru@gmail.com>
2026-04-08 22:40:46 +0530

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import SellwithUs from "./pages/SellwithUs";
 
 function App() {
 
@@ -10,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sell-berkley-estate" element={<SellwithUs />} />
       </Routes>
     </Router>
   );
