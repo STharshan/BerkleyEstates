4e035a48364ff5e9eae0293f4d870ea832715d48
DirushanDev <rubakumardirushan@gmail.com>
2026-04-10 16:10:41 +0530

    add new compontet : :

--- src/pages/Property-management.jsx
+++ src/pages/Property-management.jsx
@@ -2,6 +2,7 @@ import HeroSection from "../Common/Hero"
 import AboutSection from "../Common/About"
 import TrustedPartnerSection from "../Common/TrustedPartnerSection"
 import LetsTalkSection from "../components/Property-management/LetsTalkSection"
+import PropertyExpertiseSection from "../components/Property-management/PropertyExpertiseSection"
 const Propertymanagement = () => {
     return (
         <div>
@@ -16,6 +17,7 @@ const Propertymanagement = () => {
           {/* Injecting data using pageId */}
           <TrustedPartnerSection pageId="Propertymanagement" />
            <LetsTalkSection />
+           <PropertyExpertiseSection />
         </div>
     )
 }
