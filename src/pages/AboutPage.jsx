3ef0fb7fb33d10ad5691154781b047865b90af10
shandiru <msddiru@gmail.com>
2026-04-11 18:46:03 +0530

    all 7pages layput completed

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,10 @@ import HeroSection from "../Common/Hero"
 import AboutSection from "../Common/About"
 import TrustedPartnerSection from "../Common/TrustedPartnerSection"
 import TestimonialSection from "../Common/TestimonialSection"
+import TeamMember from "../components/AboutPage/TeamMember"
+import SaleTeam from "../components/AboutPage/SaleTeam"
+import LettingsTeam from "../components/AboutPage/LettingsTeam"
+import BerkleyEstatesSection from "../components/AboutPage/BerkleyEstatesSection"
 const AboutPage = () => {
     return (
         <div>
@@ -14,8 +18,11 @@ const AboutPage = () => {
           <AboutSection id="Aboutus" />
 
          <TrustedPartnerSection pageId="aboutus" />
-          
+          <TeamMember />
+          <SaleTeam />
+          <LettingsTeam />
        <TestimonialSection pageId="aboutus" />
+       <BerkleyEstatesSection />
         </div>
     )
 }
